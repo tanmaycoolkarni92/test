897f2b7960203b8dff3c8c7a05b10ff7e849340e
tanmaycoolkarni92 <tanmaycoolkarni@gmail.com>
2018-06-26 00:22:03 +0530

    code added to compile

--- test.java
+++ test.java
@@ -3,8 +3,10 @@ public static void main(String args[]){
     int n=5;
     for (int i=0;i<=n;i++){
         for (int j=0;j<=n;j++){
+            if((i==0||i==n)||(j==0||j==n))
             System.out.print("*");
         }
+        System.out.println();
     
   }
 }
